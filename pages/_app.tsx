a8c68fa037835f5a09d2df06b12f0f608bfced20
Tailine Chagas <tailinechagas@gmail.com>
2021-01-30 18:32:42 -0300

    feat: responsive registration form

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { createBreakpoints } from "@chakra-ui/theme-tools";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const breakpoints = createBreakpoints({
+    sm: "30em",
+    md: "48em",
+    lg: "62em",
+    xl: "80em",
+    xxl: "94em",
+  });
+
   const theme = extendTheme({
     colors: {
       purple: {
@@ -12,6 +21,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       black: "#2D2D2D",
       pink: "#F4ABC4",
     },
+    breakpoints,
   });
 
   return (
--- pages/sign_up.tsx
+++ pages/sign_up.tsx
@@ -1,110 +1,263 @@
+import React, { FormEvent, useState } from "react";
+import Image from "next/image";
+import styles from "../styles/sign_up.module.scss";
+
 import {
-  Center,
   FormControl,
   FormLabel,
   Input,
   Flex,
-  Text,
-  HStack,
+  Select,
   Button,
+  Heading,
   FormErrorMessage,
+  Grid,
+  GridItem,
+  Box,
 } from "@chakra-ui/react";
-import SelectSearch from "react-select-search";
+
+interface FormFields {
+  name: string;
+  email: string;
+  phone: string;
+  state: string;
+  city: string;
+  password: string;
+  confirmPassword: string;
+}
+
+type FormErrors = Record<keyof FormFields, boolean>;
+
+const initalFormValues: FormFields = {
+  name: "",
+  email: "",
+  phone: "",
+  state: "",
+  city: "",
+  password: "",
+  confirmPassword: "",
+};
 
 export default function SignUp() {
-  const options = [
-    { name: "Swedish", value: "sv" },
-    { name: "English", value: "en" },
-    {
-      type: "group",
-      name: "Group name",
-      items: [{ name: "Spanish", value: "es" }],
-    },
-  ];
+  const [formValues, setFormValues] = useState<FormFields>(initalFormValues);
+  const [errors, setErrors] = useState<FormErrors | undefined>();
+
+  function updateFormValue(key: keyof FormFields, value: string) {
+    setFormValues({ ...formValues, [key]: value });
+  }
+
+  function formHasErrors() {
+    const errors: FormErrors = {} as FormErrors;
+
+    for (const key of Object.keys(formValues)) {
+      if (!formValues[key].trim()) {
+        errors[key] = true;
+      }
+    }
+
+    setErrors(errors);
+    return !!Object.values(errors);
+  }
+
+  function submitForm(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    if (formHasErrors()) {
+      return;
+    }
+  }
+
   return (
-    <Flex
-      align="center"
-      direction="column"
-      w="100vw"
-      minH="100vh"
-      bg="pink"
-      p="2em 0"
-    >
-      <Text
-        w="70%"
-        textAlign="left"
-        fontSize="2em"
-        fontWeight="bold"
-        opacity=".65"
-        color="purple.900"
-        mb="1em"
-      >
-        Cadastre-se e ajude a quem precisa
-      </Text>
+    <Flex w="100vw" bg="pink" justify="center">
       <Flex
-        direction="column"
-        borderRadius="8px"
-        w="90%"
-        h="auto"
-        p="1em"
-        bg="white"
+        align="center"
+        direction={{ base: "column", lg: "row" }}
+        justifyContent="center"
+        w="100%"
+        minH="100vh"
+        p={{ base: "2em 0", lg: "3em" }}
       >
-        <form>
-          <FormControl id="name" m="1.5em 0" isRequired>
-            <FormLabel>Nome:</FormLabel>
-            <Input placeholder="Ana" />
-            <FormErrorMessage>Campo obrigatorio</FormErrorMessage>
-          </FormControl>
-
-          <FormControl id="email" m="1.5em 0" isRequired>
-            <FormLabel>Email:</FormLabel>
-            <Input type="email" placeholder="ana@gmail.com" />
-          </FormControl>
-
-          <FormControl id="phone" m="1.5em 0" isRequired>
-            <FormLabel>Telefone:</FormLabel>
-            <Input placeholder="(11) 99999-9999" />
-          </FormControl>
-
-          <FormControl id="state" m="1.5em 0" isRequired>
-            <FormLabel>Estado:</FormLabel>
-            <Input placeholder="Selecione seu estado" />
-          </FormControl>
-
-          <FormControl id="city" m="1.5em 0" isRequired>
-            <FormLabel>Cidade:</FormLabel>
-            <SelectSearch
-              options={options}
-              value="sv"
-              name="language"
-              placeholder="Choose your language"
-            />
-
-            {/* <Input placeholder="Selecione sua cidade" /> */}
-          </FormControl>
-
-          <FormControl id="password" m="1.5em 0" isRequired>
-            <FormLabel>Senha:</FormLabel>
-            <Input type="password" placeholder="Insira uma senha" />
-          </FormControl>
-
-          <FormControl id="confirmPassword" m="1.5em 0" isRequired>
-            <FormLabel>Confirmar senha:</FormLabel>
-            <Input
-              type="confirmPassword"
-              placeholder="Insira novamente a senha"
-            />
-          </FormControl>
-
-          <Button
-            textTransform="uppercase"
-            color="white"
-            w="100%"
-            bg="purple.100"
+        <Flex direction="column">
+          <Heading
+            as="h1"
+            fontSize={{ base: "2rem", xl: "3rem" }}
+            w={{ xxl: "70%" }}
+            textAlign="left"
+            fontWeight="bold"
+            opacity=".65"
+            color="purple.900"
+            mb="1em"
+            p="0 1em"
           >
-            Confirmar
-          </Button>
-        </form>
+            Cadastre-se e ajude a quem precisa
+          </Heading>
+          <div className={styles.imageContainer}>
+            <Image
+              src="/donations.svg"
+              alt="ilustração de doadores"
+              width="600"
+              height="600"
+            />
+          </div>
+        </Flex>
+        <Flex
+          direction="column"
+          borderRadius="8px"
+          h="auto"
+          maxWidth="50em"
+          w="90%"
+          p="1em"
+          bg="white"
+        >
+          <form onSubmit={submitForm}>
+            <Grid
+              templateColumns={{ md: "1fr 1fr" }}
+              gap={{ base: "0 0", md: "1.5em" }}
+              p={{ md: "2em" }}
+            >
+              <GridItem colSpan={{ base: 2, md: "auto" }}>
+                <FormControl
+                  id="name"
+                  m={{ base: "1em 0", lg: 0 }}
+                  isInvalid={errors?.name}
+                >
+                  <FormLabel>Nome:</FormLabel>
+                  <Input
+                    placeholder="Ana"
+                    onChange={({ target: { value } }) =>
+                      updateFormValue("name", value)
+                    }
+                  />
+                  <FormErrorMessage>Campo obrigatório</FormErrorMessage>
+                </FormControl>
+              </GridItem>
+
+              <GridItem colSpan={{ base: 2, md: "auto" }}>
+                <FormControl
+                  id="phone"
+                  m={{ base: "1em 0", lg: 0 }}
+                  isInvalid={errors?.phone}
+                >
+                  <FormLabel>Telefone:</FormLabel>
+                  <Input
+                    placeholder="(11) 99999-9999"
+                    onChange={({ target: { value } }) =>
+                      updateFormValue("phone", value)
+                    }
+                  />
+                  <FormErrorMessage>Campo obrigatório</FormErrorMessage>
+                </FormControl>
+              </GridItem>
+
+              <GridItem colSpan={2}>
+                <FormControl
+                  id="email"
+                  m={{ base: "1em 0", lg: 0 }}
+                  isInvalid={errors?.email}
+                >
+                  <FormLabel>Email:</FormLabel>
+                  <Input
+                    type="email"
+                    placeholder="ana@gmail.com"
+                    onChange={({ target: { value } }) =>
+                      updateFormValue("name", value)
+                    }
+                  />
+                  <FormErrorMessage>Campo obrigatório</FormErrorMessage>
+                </FormControl>
+              </GridItem>
+
+              <GridItem colSpan={{ base: 2, md: "auto" }}>
+                <FormControl
+                  id="state"
+                  m={{ base: "1em 0", lg: 0 }}
+                  isInvalid={errors?.state}
+                >
+                  <FormLabel>Estado:</FormLabel>
+                  <Select
+                    placeholder="Selecione seu estado"
+                    onChange={({ target: { value } }) =>
+                      updateFormValue("state", value)
+                    }
+                  />
+                  <FormErrorMessage>Campo obrigatório</FormErrorMessage>
+                </FormControl>
+              </GridItem>
+
+              <GridItem colSpan={{ base: 2, md: "auto" }}>
+                <FormControl
+                  id="city"
+                  m={{ base: "1em 0", lg: 0 }}
+                  isInvalid={errors?.city}
+                >
+                  <FormLabel>Cidade:</FormLabel>
+                  <Input
+                    placeholder="Selecione sua cidade"
+                    onChange={({ target: { value } }) =>
+                      updateFormValue("city", value)
+                    }
+                  />
+                  <FormErrorMessage>Campo obrigatório</FormErrorMessage>
+                </FormControl>
+              </GridItem>
+
+              <GridItem colSpan={{ base: 2, md: "auto" }}>
+                <FormControl
+                  id="password"
+                  m={{ base: "1em 0", lg: 0 }}
+                  isInvalid={errors?.password}
+                >
+                  <FormLabel>Senha:</FormLabel>
+                  <Input
+                    type="password"
+                    placeholder="Insira uma senha"
+                    onChange={({ target: { value } }) =>
+                      updateFormValue("password", value)
+                    }
+                  />
+                  <FormErrorMessage>Campo obrigatório</FormErrorMessage>
+                </FormControl>
+              </GridItem>
+
+              <GridItem colSpan={{ base: 2, md: "auto" }}>
+                <FormControl
+                  id="confirmPassword"
+                  m={{ base: "1em 0", lg: 0 }}
+                  isInvalid={errors?.confirmPassword}
+                >
+                  <FormLabel>Confirmar senha:</FormLabel>
+                  <Input
+                    type="password"
+                    placeholder="Insira novamente a senha"
+                    onChange={({ target: { value } }) =>
+                      updateFormValue("confirmPassword", value)
+                    }
+                  />
+                  <FormErrorMessage>Campo obrigatório</FormErrorMessage>
+                </FormControl>
+              </GridItem>
+
+              <GridItem colSpan={{ base: 2, md: "auto" }}>
+                <Button
+                  mb={{ base: "1.5em", lg: 0 }}
+                  mt="1.5em"
+                  type="submit"
+                  textTransform="uppercase"
+                  color="white"
+                  w="100%"
+                  bg="purple.100"
+                  _hover={{
+                    bg: "purple.100",
+                  }}
+                  _active={{
+                    bg: "purple.100",
+                  }}
+                >
+                  Confirmar
+                </Button>
+              </GridItem>
+            </Grid>
+          </form>
+        </Flex>
       </Flex>
     </Flex>
   );
